f6ce7897a25682812b4716f009078067f4618da6
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2025-06-04 12:56:57 -0300

    initial commit

---
+++ react-hooks/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import { StateExample } from './hooks/use-state'
+
+function App() {
+
+  return (
+    <>
+      <StateExample />
+    </>
+  )
+}
+
+export default App
---
+++ react-hooks/src/hooks/use-effect/index.jsx
@@ -0,0 +1,3 @@
+export const EffectExample = () => {
+    return;
+}
